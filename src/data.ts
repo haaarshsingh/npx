67c8991b052ef3595c1c9192e70925f0f2266dc7
Harsh Singh <hi.harsh@protonmail.ch>
2024-10-30 16:46:26 -0400

    change email/links

--- src/data.ts
+++ src/data.ts
@@ -2,16 +2,14 @@ import chalk from 'chalk'
 
 export const data = {
   name: chalk.bold.white('Harsh Singh'),
-  tagline: chalk.bold.grey('Developer, Designer & Student'),
-  location: chalk.bold.grey('Dallas-Fort Worth, Texas'),
-  twitter: chalk.hex('#08a0e9')('https://twitter.com/harshhhdev/'),
-  github: chalk.white('https://github.com/harshhhdev/'),
-  dribbble: chalk.hex('#EA4C89')('https://dribbble.com/harshhhdev'),
-  linkedin: chalk.hex('#0077B5')('https://linkedin.com/in/harshhhdev'),
+  tagline: chalk.bold.grey('Software and design consultant'),
+  location: chalk.bold.grey('Dallas, Texas'),
+  twitter: chalk.hex('#08a0e9')('https://twitter.com/haaarshsingh/'),
+  github: chalk.white('https://github.com/haaarshsingh/'),
+  linkedin: chalk.hex('#0077B5')('https://linkedin.com/in/haaarshsingh'),
   website: chalk.white('https://harshsingh.xyz/'),
   labelWebsite: chalk.white.bold('Website:'),
   labelTwitter: chalk.hex('#08a0e9').bold('Twitter:'),
   labelGitHub: chalk.hex('#F6F6F6').bold('GitHub:'),
-  labelDribbble: chalk.hex('#EA4C89').bold('Dribbble:'),
   labelLinkedin: chalk.hex('#0077B5').bold('Linkedin:')
 }
