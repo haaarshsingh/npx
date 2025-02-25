deb4603e2f2afc20d56fd81f2b51b41e0893739a
Harsh Singh <hi.harsh@protonmail.ch>
2025-02-25 06:29:38 -0500

    fix: link

--- src/data.ts
+++ src/data.ts
@@ -7,7 +7,7 @@ export const data = {
   twitter: chalk.hex('#08a0e9')('https://twitter.com/haaarshsingh/'),
   github: chalk.white('https://github.com/haaarshsingh/'),
   linkedin: chalk.hex('#0077B5')('https://linkedin.com/in/haaarshsingh'),
-  website: chalk.white('https://harshsingh.xyz/'),
+  website: chalk.white('https://harshsingh.me/'),
   labelWebsite: chalk.white.bold('Website:'),
   labelTwitter: chalk.hex('#08a0e9').bold('Twitter:'),
   labelGitHub: chalk.hex('#F6F6F6').bold('GitHub:'),
